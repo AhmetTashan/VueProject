7095cad6bed69b8adb2aed12496345e8ff7972f9
ahmet.tashan <ahmettashan@outlook.com>
2022-09-13 09:21:19 +0300

    fonksiyondan kurtarıldı

--- src/services/secureLs.service.js
+++ src/services/secureLs.service.js
@@ -1,9 +1,7 @@
 import secureLs from 'secure-ls'
 
-export default () => {
-    return new secureLs({
-        encodingType: 'aes',
-        encryptionSecret: process.env.VUE_APP_SECRET_KEY ?? 's3cr3tPa$$w0rd@123',
-        isCompression: true,
-    });
-}
+export default new secureLs({
+    encodingType: 'aes',
+    encryptionSecret: process.env.VUE_APP_SECRET_KEY ?? 's3cr3tPa$$w0rd@123',
+    isCompression: true,
+});
